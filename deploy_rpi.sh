c20edf803f2c9f4a8439aa057df056536d782c0f
rachanamp18-tech <your-github-email>
2026-06-08 15:05:42 +0530

    Exclude venv in deploy_rpi.sh and automate native virtualenv creation on Raspberry Pi

--- deploy_rpi.sh
+++ deploy_rpi.sh
@@ -9,9 +9,10 @@ CAMERA_DEVICE="${CAMERA_DEVICE:-}"
 echo "Deploying project to ${RPI_HOST}:${TARGET_DIR}"
 rsync -av --delete \
   --exclude '.git' \
+  --exclude 'venv' \
   --exclude '__pycache__' \
   --exclude '.pytest_cache' \
   --exclude 'audio/*.wav' \
   . "${RPI_HOST}:${TARGET_DIR}/"
 
-ssh "${RPI_HOST}" "cd ${TARGET_DIR} && ./venv/bin/python -m pip install -r requirements.txt && CAMERA_INDEX='${CAMERA_INDEX}' CAMERA_DEVICE='${CAMERA_DEVICE}' ./venv/bin/python stream_server.py"
+ssh "${RPI_HOST}" "cd ${TARGET_DIR} && ( [ -d venv ] || python3 -m venv venv ) && ./venv/bin/pip install -r requirements.txt && CAMERA_INDEX='${CAMERA_INDEX}' CAMERA_DEVICE='${CAMERA_DEVICE}' ./venv/bin/python stream_server.py"
